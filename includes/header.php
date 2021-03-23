79ffcbf71e970c0fb8d406063a0ecb4f1da14eea
Julian <cheo-93@hotmail.com>
2021-03-23 16:41:46 -0500

    header & footer
    Separacion de header, footer y contenido

---
+++ includes/header.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <!-- Titulo & icono de pestaña -->
+    <title>Mecato Valluno</title>
+    <link href="../loginppg42/includes/images/logotipo.PNG" rel="shortcut icon"/>
+</head>
+
+<body>
+
+  <p></p>  <!-- Espacio inicial -->
+
+  <!-- Header -->
+  <div class="container">
+    <div class="row-justify">
+      <div class="col-sm-12 col-xs-12">
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img src="../loginppg42/includes/images/logotipo.PNG" alt="" width="70" height="70">
+            </a>
+            <div class="collapse navbar-collapse" id="navbarText">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active fs-1 fw-italic" href="#">MECATO VALLUNO</a>
+                </li>
+              </ul>
+              <span class="navbar-text">
+                Usuario que este usando el sistema
+              </span>
+            </div>
+          </div>
+        </nav>
+
+      </div> <!--col-sm-12-->      
+    </div> <!--row-->
+  </div> <!--container MAYOR-->
+
+  <p></p>  <!-- Espacio inicial -->
+
+  <!-- Fin Header -->
---
+++ menuAdminSis.php
@@ -0,0 +1,93 @@
+<!-- solicito el header -->
+<?php include("includes/header.php") ?>
+
+<!-- body de la pagina -->
+
+<!-- menu -->
+<div class="container">
+  <div class="row-justify">
+    <div class="col-sm-12 col-xs-12">
+      <div class="card text-center">
+
+        <nav class="navbar navbar-dark bg-dark">
+          <form class="container-fluid justify-content-center">
+            <li class="nav-item dropdown">
+              <a class="btn btn-outline-light" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Personal
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">Crear</a></li>
+                <li><a class="dropdown-item" href="#">Buscar</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="btn btn-outline-light" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Cliente
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">Crear</a></li>
+                <li><a class="dropdown-item" href="#">Buscar</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="btn btn-outline-light" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Roles
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">Crear</a></li>
+                <li><a class="dropdown-item" href="#">Asigar Rol</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="btn btn-outline-light" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Gestion Tiendas
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">Crear Pais</a></li>
+                <li><a class="dropdown-item" href="#">Crear Ciudad</a></li>
+                <li><a class="dropdown-item" href="#">Crear Sede</a></li>
+              </ul>
+            </li>
+        </nav>
+
+      </div>
+      <!--col-sm-12-->
+    </div>
+    <!--row-->
+  </div>
+  <!--container MAYOR-->
+
+  <p></p>
+  <!-- contenido -->
+<div class="container">
+  <div class="row-justify">
+    <div class="col-sm-12 col-xs-12">
+      <div class="card text-center">
+
+      <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="../loginppg42/includes/images/logomv.PNG" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+      </div>
+      <!--col-sm-12-->
+    </div>
+    <!--row-->
+  </div>
+  <!--container MAYOR-->
+
+
+  <!-- fin body de la pagina -->
+
+  <!-- solicito el footer -->
+  <?php include("includes/footer.php") ?>
